ba78131169dc40427bab96eed4df1ef9f1127ec1
jenifaa <jefrahman32@gmail.com>
2024-12-07 01:40:15 +0600

    added delete function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -5,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5lka3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +25,9 @@ async function run() {
     const equipmentCollection = client
       .db("equipmentDB")
       .collection("equipment");
+    const userCollection = client.db("equipmentDB").collection("users");
+
+    // db.equipmentCollection.find(query).limit(6);
 
     app.post("/equipment", async (req, res) => {
       const newEquipment = req.body;
@@ -30,6 +35,79 @@ async function run() {
       const result = await equipmentCollection.insertOne(newEquipment);
       res.send(result);
     });
+
+    app.get("/equipment", async (req, res) => {
+      const cursor = equipmentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/equipment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await equipmentCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.get("/equipment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await equipmentCollection.findOne(query);
+      res.send(result);
+    });
+
+    //USER DATA:
+
+     app.post('/users', async(req,res) =>{
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser)
+      res.send(result);
+     })
+     app.get('/users', async(req,res) =>{
+      const cursor = userCollection.find();
+      const result = await cursor.toArray()
+      res.send(result);
+     })
+
+     app.delete('/users/:id', async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+     })
+
+    app.patch('/users', async(req,res) =>{
+      const email = req.body.email;
+      const filter = {email};
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req.body?.lastSignInTime
+        }
+      }
+      const result = await userCollection.updateOne(filter.updatedDoc)
+      res.send(result)
+    })
+
+    // app.put('/users/:id', async(req, res) =>{
+    //   const id = req.params.id;
+    //   const updatedUser = req.body;
+    // const filter = {_id: new ObjectId(id)}
+    // const option = {upsert: true}
+    // const updatePerson = {
+    //   $set: {
+    //     name: user.name,
+    //     email: user.email
+    //   }
+    // }
+    // const result = await userCollection.updateOne(filter,updatePerson, option)
+    // res.send(result);
+
+    // })
+
+
+
+
+         //END UserData
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
